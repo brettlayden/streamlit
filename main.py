840689d47b2252a0ec2e159ef839f92cd8a8d5b5
Brett Layden <brett.layden@gmail.com>
2024-04-12 15:43:20 +1000

    Try-except for missing regular/adhoc holidays

--- main.py
+++ main.py
@@ -4,17 +4,41 @@ import streamlit_calendar as sc
 
 markets = ec.get_calendar_names()
 
-market = st.selectbox("Market", markets)
+market = st.selectbox(
+    "Market", [market for market in markets if market not in ["24/5", "24/7"]]
+)
 
 calendar = ec.get_calendar(market)
-calendar_events = [
-    {
-        "title": "Ad-hoc holiday",
-        "start": f"{x.strftime('%Y-%m-%d')}",
-        "end": f"{x.strftime('%Y-%m-%d')}",
-    }
-    for x in calendar.adhoc_holidays
-]
+calendar_events = []
+
+try:
+    calendar_events.extend(
+        [
+            {
+                "title": "Ad-hoc holiday",
+                "start": f"{x.strftime('%Y-%m-%d')}",
+                "end": f"{x.strftime('%Y-%m-%d')}",
+            }
+            for x in calendar.adhoc_holidays
+        ]
+    )
+except:
+    pass
+
+
+try:
+    calendar_events.extend(
+        [
+            {
+                "title": f"{x[1]}",
+                "start": f"{x[0].strftime('%Y-%m-%d')}",
+                "end": f"{x[0].strftime('%Y-%m-%d')}",
+            }
+            for x in calendar.regular_holidays.holidays(return_name=True).items()
+        ]
+    )
+except:
+    pass
 
 calendar_options = {
     "headerToolbar": {
@@ -23,16 +47,5 @@ calendar_options = {
         "right": "dayGridMonth,multiMonthYear,listYear",
     }
 }
-
-calendar_events.extend(
-    [
-        {
-            "title": f"{x[1]}",
-            "start": f"{x[0].strftime('%Y-%m-%d')}",
-            "end": f"{x[0].strftime('%Y-%m-%d')}",
-        }
-        for x in calendar.regular_holidays.holidays(return_name=True).items()
-    ]
-)
 
 sc.calendar(events=calendar_events, options=calendar_options)
